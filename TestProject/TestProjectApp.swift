d4429604c22b4e13e32daa847ce14572a78e3663
elena.liu <elena.liu@baifu-tech.net>
2024-04-27 19:14:51 +0800

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Elena on 2024/4/27.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
